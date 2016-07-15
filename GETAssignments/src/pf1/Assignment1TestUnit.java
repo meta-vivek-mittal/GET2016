c6d5c488c73c740d787b1aa0773e5baec0af7710
meta-vivek-mittal <vivek.mittal@metacube.com>
2016-07-15 15:01:10 +0530

    PF assignment1

---
+++ GETAssignments/src/pf1/Assignment1TestUnit.java
@@ -0,0 +1,18 @@
+package Assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Assignment1TestUnit {
+
+	@Test
+	public void test() {
+
+		assertEquals(14, Assignment1.convertBinaryToOctal(1100));
+		assertEquals(65, Assignment1.convertBinaryToOctal(110101));
+		assertEquals(11, Assignment1.convertBinaryToOctal(1001));
+		assertEquals(23, Assignment1.convertBinaryToOctal(10011));
+
+	}
+}
