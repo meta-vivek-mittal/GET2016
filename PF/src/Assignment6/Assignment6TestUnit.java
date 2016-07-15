cc52b863e688be4fa3f789ee8dc258c67c6e2943
meta-vivek-mittal <vivek.mittal@metacube.com>
2016-07-15 11:57:20 +0530

    Assignment 1 Submission

---
+++ PF/src/Assignment6/Assignment6TestUnit.java
@@ -0,0 +1,18 @@
+package Assignment6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Assignment6TestUnit {
+
+	Assignment6 arraymerge = new Assignment6();
+
+	@Test
+	public void test() {
+		int array1[] = { 1, 2, 4, 6, 7, 9, 11 };
+		int array2[] = { 2, 4, 5, 7, 8, 9, 10, 13 };
+		// assertArrayValue(true,arraymerge(array1,array2));
+		assertTrue(arraymerge.check(array1, array2));
+	}
+}
