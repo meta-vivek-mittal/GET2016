a5d4d5fb012ec3e5b4c180a78b6a3b306f487438
meta-vivek-mittal <vivek.mittal@metacube.com>
2016-07-28 23:25:48 +0530

    assignment 4 Submission

---
+++ Session-1/assignment4/Node.java
@@ -0,0 +1,19 @@
+package session1.assignment4;
+
+public class Node<T> {
+	public T nodeValue;
+	public Node<T> next;
+	public Node<T> previous;
+	
+	public Node(){
+		nodeValue = null;
+		next = null;
+		previous=null;
+	}
+	
+	public Node(T item){
+		nodeValue = item;
+		next = null;
+		previous=null;
+	} 
+}
